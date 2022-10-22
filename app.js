f6e06d5df7a39ae6ab87a79cb771fe5e7a0af3cf
Hari Sharan Bhuju <haribhuju@users.noreply.github.com>
2022-10-22 19:30:24 +1100

    added edit features

--- app.js
+++ app.js
@@ -1,34 +1,45 @@
 const empty = document.querySelector(".main__empty");
 const taskLinks = document.querySelector(".main__tasks");
-const deleteBtn = document.querySelector(".main__tasks-list__delete")
+const deleteBtn = document.querySelector(".main__tasks-list__delete");
+
 
 const form = document.querySelector(".main__form");
 
-const checkEmpty = () => {  
-  (taskLinks.children.length > 0) ? empty.classList.add("empty") : empty.classList.remove("empty");;
+const checkEmpty = () => {
+    (taskLinks.children.length > 0) ? empty.classList.add("empty"): empty.classList.remove("empty");
+
 }
 
-const generateUniqueId = (input) =>{
+const generateUniqueId = (input) => {
     return Math.random().toString(36).substring(2, 2 + input);
 }
 
-const makeListElement = (inputValue) =>{
+const makeListElement = (inputValue) => {
     const uniqueId = generateUniqueId(10);
     const taskList = document.createElement("li");
     taskList.classList.add("main__tasks-list");
     taskList.innerHTML = `
         <input type="checkbox" name="new-task" id="${uniqueId}">
-        <span class="main__tasks-list__name">${inputValue}</span>
-        <button class="main__tasks-list__delete btn">
-            <img class="delete__img" width="20" height="20" src="/assets/trash-icon.svg" alt="delete icon">
-        </button>
+        <input type="text" class="main__tasks-list__name" value="${inputValue}" disabled></input>
+        <div >
+            <button class="main__tasks-list__edit btn">
+                <img class="edit__img" width="20" height="20" src="/assets/edit-icon.svg" alt="edit icon">
+            </button>
+            <button class="main__tasks-list__delete btn">
+                <img class="delete__img" width="20" height="20" src="/assets/trash-icon.svg" alt="delete icon">
+            </button> 
+        </div>
     `;
     return taskList;
 }
 
-form.addEventListener("submit", (e) =>{
+
+
+
+//add
+form.addEventListener("submit", (e) => {
     e.preventDefault();
-    
+
     //get the input value
     const newTask = document.querySelector("#newTask");
     const inputValue = newTask.value;
@@ -40,7 +51,7 @@ form.addEventListener("submit", (e) =>{
     newTask.focus();
 
     //check the input value, if empty exit the submit event listeners
-    if(!inputValue) return;
+    if (!inputValue) return;
 
     const makeElement = makeListElement(inputValue);
 
@@ -48,11 +59,28 @@ form.addEventListener("submit", (e) =>{
     checkEmpty();
 });
 
-document.addEventListener("click", (e) => {
-    if(e.target.matches('.btn') || e.target.matches('.delete__img')) {
-        (e.target.closest("li")).remove();
-        checkEmpty(); 
-    } 
-  
-});
 
+//edit
+document.addEventListener("click", (e) => {
+    if (e.target.matches('.main__tasks-list__edit') || e.target.matches('.edit__img')) {
+        listItem = e.target.closest('li');
+        for (let child of listItem.children) {
+            if (child.matches('.main__tasks-list__name')) {
+                child.disabled = false;
+                child.addEventListener("focusout", () => {
+                    child.disabled = true;
+                })
+            }
+        }
+    }
+})
+
+
+
+//delete
+document.addEventListener("click", (e) => {
+    if (e.target.matches('.main__tasks-list__delete') || e.target.matches('.delete__img')) {
+        (e.target.closest("li")).remove();
+        checkEmpty();
+    }
+});
